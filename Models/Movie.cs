e3df15b38dfcd44e72b0f3d9c290b3c4e25cd99d
KirYes <kirillkakirill78@gmail.com>
2025-04-16 00:57:06 +0300

    Added a data model, scaffold process, initial db schecma using EF's migration

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,6 +1,14 @@
-﻿namespace WebApplication2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
 {
     public class Movie
     {
+        public int ID { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
     }
 }
